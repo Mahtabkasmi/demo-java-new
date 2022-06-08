3e13245a8bd6213569435c13b5410fc90be2fe92
Mahtab Kasmi <eaimirk@gmail.com>
2022-06-08 18:11:31 +0400

    mahtab change agin hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class  hello {
+    public static void func(){
+        System.out.println("hello yr name");
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
+        func();
     }
 }
